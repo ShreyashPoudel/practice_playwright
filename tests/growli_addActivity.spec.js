6c4d3517f2821eca1817707d7f5a06fc1a86e6a0
Shreyash Poudel <poudelshreyash36@gmail.com>
2026-02-06 15:10:40 +0545

    added random function to select random options

--- tests/growli_addActivity.spec.js
+++ tests/growli_addActivity.spec.js
@@ -10,30 +10,28 @@ function getRandomItem(array) {
 }
 
 async function login(page) {
-    await page.goto('https://dev-gweb.gateway.do.webpoint.io/');
+    await page.goto(process.env.Login_URL);
     await page.getByRole('link', {name: 'Get Started'}).click();
     await page.getByRole('textbox', {name: 'Email Address'}).fill(process.env.EMAIL);
     await page.getByRole('textbox', {name: 'Password'}).fill(process.env.PASSWORD);
     await page.locator('button[type="submit"]').click();
 }
 
-    const randomTitle = getRandomItem(activityData.activityTitles);
-    const randomType = getRandomItem(activityData.activityTypes);
-    const randomAgeRange = getRandomItem(activityData.ageRanges);
-    const randomDescription = getRandomItem(activityData.activityDescriptions);
-    const randomYoutubeLink = getRandomItem(activityData.youtubeLinks);
-    const randomStep1 = getRandomItem(activityData.steps1);
-    const randomStep2 = getRandomItem(activityData.steps2);
-
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 3; i++) {
     test("Add activity " + (i + 1), async ({page}) => {
-        await login(page);
-        await page.waitForTimeout(3000);
-        await page.goto('https://dev-gweb.gateway.do.webpoint.io/admin/activities');
+        const randomTitle = getRandomItem(activityData.activityTitles);
+        const randomType = getRandomItem(activityData.activityTypes);
+        const randomAgeRange = getRandomItem(activityData.ageRanges);
+        const randomDescription = getRandomItem(activityData.activityDescriptions);
+        const randomYoutubeLink = getRandomItem(activityData.youtubeLinks);
+        const randomStep1 = getRandomItem(activityData.steps1);
+        const randomStep2 = getRandomItem(activityData.steps2);
 
-        // await page.getByRole('link', { name: 'Activities' }).click();
-        // await page.locator("(//nav[contains(@class,'flex flex-col')]//a)[2]").click();
-        // await page.getByRole('link', {name: 'Activities'}).click();
+        await login(page);
+
+        await page.waitForTimeout(3000);
+
+        await page.goto(process.env.Login_URL + 'admin/activities');
         await page.getByRole('button', {name: 'Create New'}).click();
         await page.getByRole('textbox', {name: 'Activity Title'}).fill(randomTitle);
 
@@ -41,8 +39,14 @@ for (let i = 0; i < 2; i++) {
         await page.getByRole('menuitem', {name: randomType}).click();
 
         await page.mouse.click(0, 0);
-        await page.getByRole('button', { name: 'Select age range' }).click();
+
+        await page.getByRole('button', {name: 'Select age range'}).click();
         await page.getByRole('menuitem', {name: randomAgeRange}).click();
+
+        // // Get all option values from the select dropdown
+        //     const options = await page.locator('select option').evaluateAll(opts => opts.map(o => o.value));
+        //     const randomOption = options[Math.floor(Math.random() * options.length)];
+        //     await page.locator('select').selectOption(randomOption);
 
         await page.getByRole('textbox', {name: 'Activity Description'}).fill(randomDescription);
 
