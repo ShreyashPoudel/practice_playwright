d49c194509732ed07646dde2e5a5a4cbbded9543
Shreyash Poudel <poudelshreyash36@gmail.com>
2026-03-18 17:26:57 +0545

    implemented login fucntion and navigation to adding workshop

--- tests/add_workshop.spec.js
+++ tests/add_workshop.spec.js
@@ -2,7 +2,17 @@ import{test,expect} from '@playwright/test';
 import {LoginPage} from '../pages/login.js';
 
 test("Add workshop", async ({page}) => {
+    
+    // login
     const loginPage = new LoginPage(page);
     await loginPage.login();
+    await page.waitForTimeout(2000);
+
+    // navigate to activities
+    await page.goto(process.env.URL + 'admin/activities');
+    await page.getByRole('button', {name: 'Create Activity'}).click();
+
+    await page.getByRole('button', {name: 'View Workshop'}).click();
+    await page.getByRole('button', {name: 'Create New Workshop'}).click();
 
     });
