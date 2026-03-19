04672256f4228a51deb7b25dd9834dbf8f9d7f8f
Shreyash Poudel <poudelshreyash36@gmail.com>
2026-03-19 21:35:46 +0545

    khai

--- tests/add_activity.spec.js
+++ tests/add_activity.spec.js
@@ -1,83 +1,88 @@
-import {expect, test} from '@playwright/test';
-import activityData from '../Data/activityData.json' assert {type: 'json' };
-import { ImageUpload } from '../pages/image_upload_activity';
+import { expect, test } from "@playwright/test";
+import activityData from "../Data/activityData.json" assert { type: "json" };
+import { ImageUpload } from "../pages/image_upload_activity";
 // import {getRandomItem} from '../helpers/randomFuntion';
-import { LoginPage } from '../pages/login';
+import { LoginPage } from "../pages/login";
 test.setTimeout(30000);
 
 function getRandomItem(array) {
-    return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 for (let i = 0; i < 6; i++) {
-    test("Add activity " + (i + 1), async ({page}) => {
-        const randomTitle = getRandomItem(activityData.activityTitles);
-        const randomType = getRandomItem(activityData.activityTypes);
-        const randomAgeRange = getRandomItem(activityData.ageRanges);
-        const randomDescription = getRandomItem(activityData.activityDescriptions);
-        const randomYoutubeLink = getRandomItem(activityData.youtubeLinks);
-        const randomAccessType = getRandomItem(activityData.accessType);
+  test("Add activity " + (i + 1), async ({ page }) => {
+    const randomTitle = getRandomItem(activityData.activityTitles);
+    const randomType = getRandomItem(activityData.activityTypes);
+    const randomAgeRange = getRandomItem(activityData.ageRanges);
+    const randomDescription = getRandomItem(activityData.activityDescriptions);
+    const randomYoutubeLink = getRandomItem(activityData.youtubeLinks);
+    const randomAccessType = getRandomItem(activityData.accessType);
 
-        // login
-        const loginPage = new LoginPage(page);
-        await loginPage.login();
-        await page.waitForTimeout(2000);
+    // login
+    const loginPage = new LoginPage(page);
+    await loginPage.login();
+    await page.waitForTimeout(2000);
 
-        // navigate to activities
-        await page.goto(process.env.URL + 'admin/activities');
-        await page.getByRole('button', {name: 'Create Activity'}).click();
-        await page.getByRole('textbox', {name: 'Activity Title'}).fill(randomTitle);
+    // navigate to activities
+    await page.goto(process.env.URL + "admin/activities");
+    await page.getByRole("button", { name: "Create Activity" }).click();
+    await page
+      .getByRole("textbox", { name: "Activity Title" })
+      .fill(randomTitle);
 
-        // select category
-        await page.getByRole('button', { name: 'Choose category' }).click();
-        await page.getByRole('menuitem', {name: randomType}).click();
-        await page.mouse.click(0, 0);
+    // select category
+    await page.getByRole("button", { name: "Choose category" }).click();
+    await page.getByRole("menuitem", { name: randomType }).click();
+    await page.mouse.click(0, 0);
 
-        // select age range
-        await page.getByRole('button', {name: 'Select age range'}).click();
-        await page.getByRole('menuitem', {name: randomAgeRange}).click();
+    // select age range
+    await page.getByRole("button", { name: "Select age range" }).click();
+    await page.getByRole("menuitem", { name: randomAgeRange }).click();
 
-        // fill description
-        await page.getByRole('textbox', {name: 'Activity Description'}).fill(randomDescription);
+    // fill description
+    await page
+      .getByRole("textbox", { name: "Activity Description" })
+      .fill(randomDescription);
 
-        // add media
-        const coverUpload = new ImageUpload(page);
-        await coverUpload.uploadRandomImage();
+    // add media
+    const coverUpload = new ImageUpload(page);
+    await coverUpload.uploadRandomImage();
 
-        // fill youtube link
-        await page.locator("//input[@placeholder='https://youtube.com/watch?v=...']").fill(randomYoutubeLink);
+    // fill youtube link
+    await page
+      .locator("//input[@placeholder='https://youtube.com/watch?v=...']")
+      .fill(randomYoutubeLink);
 
-        // activity access type
-        await page.getByRole('button', {name: randomAccessType}).click();
+    // activity access type
+    await page.getByRole("button", { name: randomAccessType }).click();
 
-        const allSteps = [
-            ...activityData.steps1,
-            ...activityData.steps2,
-            ...activityData.steps3
-        ];
+    const allSteps = [
+      ...activityData.steps1,
+      ...activityData.steps2,
+      ...activityData.steps3,
+    ];
 
-        allSteps.sort(() => Math.random() - 0.5);
+    allSteps.sort(() => Math.random() - 0.5);
 
-        // Random number of steps
-        const n = Math.floor(Math.random() * 6) + 1;
-        await page.getByPlaceholder('First magical step...').waitFor();
+    // Random number of steps
+    const n = Math.floor(Math.random() * 6) + 1;
+    await page.getByPlaceholder("First magical step...").waitFor();
 
-        for (let i = 0; i < n; i++) {
-            if (i === 0) {
-                await page.getByPlaceholder('First magical step...')
-                    .fill(allSteps[i]);
-            } else if (i === 1) {
-                await page.getByPlaceholder('Second wonderful step...')
-                    .fill(allSteps[i]);
-            } else {
-                await page.getByRole('button', { name: '+ Add Another Step' }).click();
-                const stepName = `Step ${i + 1}`;
-                await page.getByPlaceholder(stepName).waitFor();
-                await page.getByPlaceholder(stepName).fill(allSteps[i]);
-            }
-        }   
-        await page.waitForTimeout(1000);
-        await page.getByRole('button', {name: 'Create Activity'}).click();
-        
-    });
+    for (let i = 0; i < n; i++) {
+      if (i === 0) {
+        await page.getByPlaceholder("First magical step...").fill(allSteps[i]);
+      } else if (i === 1) {
+        await page
+          .getByPlaceholder("Second wonderful step...")
+          .fill(allSteps[i]);
+      } else {
+        await page.getByRole("button", { name: "+ Add Another Step" }).click();
+        const stepName = `Step ${i + 1}`;
+        await page.getByPlaceholder(stepName).waitFor();
+        await page.getByPlaceholder(stepName).fill(allSteps[i]);
+      }
+    }
+    await page.waitForTimeout(1000);
+    await page.getByRole("button", { name: "Create Activity" }).click();
+  });
 }
