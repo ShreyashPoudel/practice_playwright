8fc7f01a8bd6621b437008e2bb591df66ea8a43a
Shreyash Poudel <poudelshreyash36@gmail.com>
2026-05-13 14:24:57 +0545

    fix: remve age ranges

--- tests/add_activity.spec.js
+++ tests/add_activity.spec.js
@@ -8,7 +8,7 @@ function getRandomItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 20; i++) {
   test("Add activity " + (i + 1), async ({ page }) => {
     const randomTitle = getRandomItem(activityData.activityTitles);
     const randomType = getRandomItem(activityData.activityTypes);
